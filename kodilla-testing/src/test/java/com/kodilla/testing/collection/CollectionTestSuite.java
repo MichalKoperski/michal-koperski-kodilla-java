aa8a01be7869298ef2e60af35a31c3e8fda5bdd1
mikeITgithub <michal@koperski.cc>
2018-04-15 15:46:36 +0200

    m

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,66 +1,45 @@
 package com.kodilla.testing.collection;
 
 import org.junit.*;
+import org.junit.rules.TestName;
 
 import java.util.*;
 
-public class CollectionTestSuite {
-    private static int counter = 0;
+import static org.junit.Assert.assertEquals;
 
+public class CollectionTestSuite {
+    @Rule
+    public TestName name = new TestName();
     @Test
     public void testOddNumbersExterminatorEmptyList() {
-
         List<Integer> number = new ArrayList<Integer>();
         OddNumbersExterminator oddext = new OddNumbersExterminator();
-        Assert.assertEquals(0, oddext.exterminate(number).size());
+        assertEquals(0, oddext.exterminate(number).size());
     }
-
     @Test
-    public void testOddNumbersExterminatorNormalList() {
+    public void testOddNumbersExterminatorListWith10Digits() {
         List<Integer> number = new ArrayList<Integer>();
         OddNumbersExterminator oddext = new OddNumbersExterminator();
         for (int i = 0; i < 10; i++) {
             number.add(i);
         }
-        Assert.assertEquals(5, oddext.exterminate(number).size());
+        assertEquals(5, oddext.exterminate(number).size());
     }
-
     @Test
-    public void testOddNumbersExterminatorNormalList2() {
+    public void testOddNumbersExterminatorListWith100Digits() {
         List<Integer> number = new ArrayList<Integer>();
         OddNumbersExterminator oddext = new OddNumbersExterminator();
         for (int i = 100; i >= 0; i--) {
             number.add(i);
         }
-        Assert.assertEquals(51, oddext.exterminate(number).size());
+        assertEquals(51, oddext.exterminate(number).size());
     }
-
     @Before
     public void before() {
-        counter++;
-        if (counter == 1) {
-            System.out.println("Testing case when list is empty");
-        }
-        if (counter == 2) {
-            System.out.println("Testing case when list contains 10 digits");
-        }
-        if (counter == 3) {
-            System.out.println("Testing case when list contains 100 digits");
-        }
+        System.out.println("Now we test: "+name.getMethodName());
     }
-        @After
-        public void after() {
-            if (counter == 1) {
-                System.out.println("Test with empty list: pass");
-            }
-            if (counter == 2) {
-                System.out.println("Test with 10 digits list: pass");
-            }
-            if (counter == 3) {
-                System.out.println("Test with 100 digits list: pass");
-            }
-        }
+    @After
+    public void after() {
+        System.out.println("Test: "+name.getMethodName()+": passed");
     }
-
-
-
+}
