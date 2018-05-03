4cd8efd6340f111b479407402da9e5276babbe44
mikeITgithub <michal@koperski.cc>
2018-05-03 21:17:05 +0200

    m

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/PurchaseProcessor.java
+++
@@ -1,8 +0,0 @@
-package com.kodilla.good.patterns.food2door;
-
-public class PurchaseProcessor {
-    public void process(String producerName) {
-        System.out.println("Thanks for buying from "+producerName);
-    }
-
-}
---
+++ kodilla-rps/src/test/java/com/kodilla/rps/RPSTestSuite.java
@@ -0,0 +1,48 @@
+package com.kodilla.rps;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class RPSTestSuite {
+
+    private Player player = mock(Player.class);
+    private Computer computer = mock(Computer.class);
+    private GameOutcome gameOutcome = new GameOutcome(player,computer,1);
+
+    @Test
+    public void player1Wins() {
+
+
+        when(player.makeMove()).thenReturn("2");
+        when(computer.makeMove()).thenReturn("1");
+
+        int result = gameOutcome.play().getPlayer1Points();
+
+        Assert.assertEquals(1, result);
+    }
+
+    @Test
+    public  void player2Wins() {
+
+        when(player.makeMove()).thenReturn("1");
+        when(computer.makeMove()).thenReturn("2");
+
+        int result = gameOutcome.play().getPlayer1Points();
+
+        Assert.assertEquals(0, result);
+    }
+
+    @Test
+    public  void playerEndsGame() {
+
+        when(player.makeMove()).thenReturn("x");
+        when(computer.makeMove()).thenReturn("2");
+
+        int result = gameOutcome.play().getPlayer1Points();
+
+        Assert.assertEquals(0, result);
+    }
+}
