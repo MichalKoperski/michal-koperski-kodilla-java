c042199409e7f58dff362ca63b0c737edba5928b
mikeITgithub <michal@koperski.cc>
2018-05-11 19:05:09 +0200

    m

--- kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Component;
 public class ForumUser {
    private String username;
 
+    public ForumUser(String username) {
+        this.username = username;
+    }
+
     public String getUsername() {
         return "John Smith";
     }
