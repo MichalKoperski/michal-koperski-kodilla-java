ee114a8a56bf2232ed6157d932e88c88187f045a
MichalKoperski <michal@koperski.cc>
2018-07-22 14:29:15 +0200

    m

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/dao/calculator/Watcher.java
@@ -0,0 +1,18 @@
+package com.kodilla.patterns2.dao.calculator;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class Watcher {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Watcher.class);
+
+    @Before("execution(* com.kodilla.patterns2.aop.calculator.Calculator.factorial(..))")
+    public void logEvent() {
+        LOGGER.info("Logging the event");
+    }
+}
