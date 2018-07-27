a62b035a595194755799006ee4bd0ec256944398
MichalKoperski <michal@koperski.cc>
2018-07-27 19:56:29 +0200

    m

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/facade/FacadeSearchTest.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/facade/FacadeSearchTest.java
@@ -2,8 +2,6 @@ package com.kodilla.hibernate.manytomany.facade;
 
 import com.kodilla.hibernate.manytomany.Company;
 import com.kodilla.hibernate.manytomany.Employee;
-import com.kodilla.hibernate.manytomany.dao.CompanyDao;
-import com.kodilla.hibernate.manytomany.dao.EmployeeDao;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,11 +16,6 @@ import java.util.List;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FacadeSearchTest {
-    @Autowired
-    CompanyDao companyDao;
-
-    @Autowired
-    EmployeeDao employeeDao;
 
     @Autowired
     SearchFacade searchFacade;
